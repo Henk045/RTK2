85536469f3dafef90922c1fe8723959541c01150
Henk045 <76645026+Henk045@users.noreply.github.com>
2024-11-26 00:59:17 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ window.addEventListener("scroll", function () {
     const topNavigation = document.querySelector(".topnav");
     const header = document.querySelector(".header");
 
-    // if mobile, don't do this
     if (window.innerWidth < 768) {
         console.log("Mobile detected");
         return;
@@ -11,23 +10,23 @@ window.addEventListener("scroll", function () {
 
     if (window.scrollY > header.offsetHeight) {
         topNavigation.classList.add("fixed");
-        console.log("Thick latina")
     } else {
         topNavigation.classList.remove("fixed");
-        console.log("Skinny latina")
     }
 })
 
 
 
-// when clicking on an image with class 'photo', make it 100% width, if it's already 100% width, remove it
-document.querySelectorAll(".photo-frame").forEach(function (photoFrame) {
-    photoFrame.addEventListener("click", function () {
-        console.log("clicked on photo");
-        if (photoFrame.classList.contains("full-width")) {
-            photoFrame.classList.remove("full-width");
-        } else {
-            photoFrame.classList.add("full-width");
-        }
-    })
-});
+if (window.innerWidth >= 768) {
+    document.querySelectorAll(".photo-frame").forEach(function (photoFrame) {
+        photoFrame.addEventListener("click", function () {
+            console.log("clicked on photo");
+            if (photoFrame.classList.contains("full-width")) {
+                photoFrame.classList.remove("full-width");
+            } else {
+                photoFrame.classList.add("full-width");
+            }
+        })
+    });
+}
+
