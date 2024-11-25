67b230d2fad4075b81e4caeedce50b6f45c7b7a5
Henk045 <76645026+Henk045@users.noreply.github.com>
2024-11-25 23:36:13 +0100

    changes phone

--- script.js
+++ script.js
@@ -16,17 +16,17 @@ window.addEventListener("scroll", function () {
 })
 
 
-document.querySelectorAll(".photo-frame").forEach(function (photoFrame) {
-    photoFrame.addEventListener("click", function () {
-        if (window.innerWidth < 768) {
-            console.log("Ik doe lekker niets...");
-            return;
-        }
-        if (photoFrame.classList.contains("full-width")) {
-            photoFrame.classList.remove("full-width");
-        }
-        else {
-            photoFrame.classList.add("full-width");
-        }
-    })
-});
+
+if (window.innerWidth >= 768) {
+    document.querySelectorAll(".photo-frame").forEach(function (photoFrame) {
+        photoFrame.addEventListener("click", function () {
+            console.log("clicked on photo");
+            if (photoFrame.classList.contains("full-width")) {
+                photoFrame.classList.remove("full-width");
+            } else {
+                photoFrame.classList.add("full-width");
+            }
+        })
+    });
+}
+
