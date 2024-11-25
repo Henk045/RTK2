daa6f98b2eeb076a8306b82189ea37444aa7257e
Henk045 <76645026+Henk045@users.noreply.github.com>
2024-11-25 23:28:55 +0100

    changes

--- script.js
+++ script.js
@@ -10,21 +10,22 @@ window.addEventListener("scroll", function () {
 
     if (window.scrollY > header.offsetHeight) {
         topNavigation.classList.add("fixed");
-        console.log("Thick latina")
     } else {
         topNavigation.classList.remove("fixed");
-        console.log("Skinny latina")
     }
 })
 
 
-
 document.querySelectorAll(".photo-frame").forEach(function (photoFrame) {
     photoFrame.addEventListener("click", function () {
-        console.log("clicked on photo");
+        if (window.innerWidth < 768) {
+            console.log("Ik doe lekker niets...");
+            return;
+        }
         if (photoFrame.classList.contains("full-width")) {
             photoFrame.classList.remove("full-width");
-        } else {
+        }
+        else {
             photoFrame.classList.add("full-width");
         }
     })
